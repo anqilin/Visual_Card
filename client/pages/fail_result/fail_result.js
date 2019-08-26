6ec313c080877d4f480bc957cc944ff05be0d42d
anqilin <anqilin2009@163.com>
2019-08-26 16:07:28 +0800

    0826版本

---
+++ client/pages/fail_result/fail_result.js
@@ -0,0 +1,42 @@
+const app= getApp();
+Page({
+  data: {
+    systemInfo: {} ,// 手机基础信息
+    userInfo:{},
+    userLogin:false,
+    ueerNotLogin:true,
+    cardInfo:{},
+
+
+  },
+  onLoad(options) {
+    try {
+      // 获取手机基础信息(头状态栏和标题栏高度)
+      let systemInfo = my.getSystemInfoSync();
+      this.setData({ systemInfo });
+    } catch (e) {
+      console.log(e);
+      my.alert({
+        title: '温馨提示',
+        content: 'onLoad 执行异常'
+      });
+    }
+
+    my.hideFavoriteMenu();
+
+
+  },
+
+  /**
+   * 点击手机标题栏触发的事件,需要在index.json配置titlePenetrate:"YES"
+   * @method onTitleBar
+   */
+  onTitleBar(e) {
+    /*my.alert({
+      title: '温馨提示',
+      content: '您点击了"我是手机标题栏"'
+    });*/
+  },
+
+  
+});
---
+++ client/pages/recharge_card/recharge_card.js
@@ -0,0 +1,122 @@
+const app= getApp();
+Page({
+  data: {
+    systemInfo: {} ,// 手机基础信息
+    userInfo:{},
+    userLogin:false,
+    ueerNotLogin:true,
+    cardInfo:{},
+    SERVER_URL : "https://online.sptcc.com:8445/",
+    color1:"#ffffff",
+    color2:"#ffffff",
+    color3:"#ffffff",
+    color4:"#ffffff",
+    color5:"#ffffff",
+    color6:"#ffffff",
+    text_color1:"#18BB99",
+    text_color2:"#18BB99",
+    text_color3:"#18BB99",
+    text_color4:"#18BB99",
+    text_color5:"#18BB99",
+    text_color6:"#18BB99",
+  },
+  onLoad(options) {
+    try {
+      // 获取手机基础信息(头状态栏和标题栏高度)
+      let systemInfo = my.getSystemInfoSync();
+      this.setData({ systemInfo });
+    } catch (e) {
+      console.log(e);
+      my.alert({
+        title: '温馨提示',
+        content: 'onLoad 执行异常'
+      });
+    }
+
+    my.hideFavoriteMenu();
+
+
+  },
+
+  /**
+   * 点击手机标题栏触发的事件,需要在index.json配置titlePenetrate:"YES"
+   * @method onTitleBar
+   */
+  onTitleBar(e) {
+    /*my.alert({
+      title: '温馨提示',
+      content: '您点击了"我是手机标题栏"'
+    });*/
+  },
+  onsetmoney(e){
+      console.log(e.target.dataset.money);
+      var money=e.target.dataset.money;
+
+      this.setData({
+          color1:"#ffffff",
+          color2:"#ffffff",
+          color3:"#ffffff",
+          color4:"#ffffff",
+          color5:"#ffffff",
+          color6:"#ffffff",
+          text_color1:"#18BB99",
+          text_color2:"#18BB99",
+          text_color3:"#18BB99",
+          text_color4:"#18BB99",
+          text_color5:"#18BB99",
+          text_color6:"#18BB99",
+      });
+
+ 
+      if(money=="10"){
+        this.setData({
+          color1:"#06B794",
+          text_color1:"#ffffff"
+        });
+      }else if(money=="20"){
+        this.setData({
+          color2:"#06B794",
+          text_color2:"#ffffff"
+        });
+
+      }else if(money=="30"){
+        this.setData({
+          color3:"#06B794",
+          text_color3:"#ffffff"
+        });
+
+      }else if(money=="100"){
+        this.setData({
+          color4:"#06B794",
+          text_color4:"#ffffff"
+        });
+
+      }else if(money=="200"){
+        this.setData({
+          color5:"#06B794",
+          text_color5:"#ffffff"
+        });
+
+      }else if(money=="500"){
+        this.setData({
+          color6:"#06B794",
+          text_color6:"#ffffff"
+        });
+
+      }
+
+      
+      
+  },
+
+
+
+
+
+
+
+
+
+
+
+});
