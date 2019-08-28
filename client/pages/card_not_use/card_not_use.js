e2288a3976f0c89780c01e5532ef28d066efe49b
anqilin <anqilin2009@163.com>
2019-08-28 20:48:25 +0800

    0828版本2

---
+++ client/pages/card_not_use/card_not_use.js
@@ -0,0 +1,22 @@
+import absolute from '/util/huawei';
+Page({
+  data: {
+    systemInfo: {},
+    keyi:true
+  },
+    onLoad(options) {
+    try {
+      // 获取手机基础信息(头状态栏和标题栏高度)
+      let systemInfo = my.getSystemInfoSync();
+      this.setData({ systemInfo });
+    } catch (e) {
+      console.log(e);
+      my.alert({
+        title: '温馨提示',
+        content: 'onLoad 执行异常'
+      });
+    }
+     my.hideFavoriteMenu();
+
+  },
+});
---
+++ client/pages/creat_card/bind_fail/bind_fail.js
@@ -0,0 +1,23 @@
+import absolute from '/util/huawei';
+Page({
+  data: {
+    systemInfo: {},
+    keyi:true,
+    time:"2019-10-15 12:34"
+  },
+    onLoad(options) {
+    try {
+      // 获取手机基础信息(头状态栏和标题栏高度)
+      let systemInfo = my.getSystemInfoSync();
+      this.setData({ systemInfo });
+    } catch (e) {
+      console.log(e);
+      my.alert({
+        title: '温馨提示',
+        content: 'onLoad 执行异常'
+      });
+    }
+     my.hideFavoriteMenu();
+
+  },
+});
