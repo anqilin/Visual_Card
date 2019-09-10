e21a0df7699a0eb14ed0c5af16dd034296c1f5f7
anqilin <anqilin2009@163.com>
2019-09-10 21:48:34 +0800

    091002版本

---
+++ client/pages/refund_result/refund_result.js
@@ -0,0 +1,57 @@
+const app= getApp();
+Page({
+  data: {
+    systemInfo: {} ,// 手机基础信息
+    userInfo:{},
+    userLogin:false,
+    ueerNotLogin:true,
+    cardInfo:{},
+    from:0,
+
+  },
+  onLoad(options) {
+    try {
+      // 获取手机基础信息(头状态栏和标题栏高度)
+      //let systemInfo = my.getSystemInfoSync();
+      let systemInfo=app.systemInfo;
+      this.setData({ systemInfo });
+    } catch (e) {
+      app.log(e);
+      my.alert({
+        title: '温馨提示',
+        content: 'onLoad 执行异常'
+      });
+    }
+
+    my.hideFavoriteMenu();
+    
+
+
+  },
+  onShow(){
+    
+
+
+  },
+
+  /**
+   * 点击手机标题栏触发的事件,需要在index.json配置titlePenetrate:"YES"
+   * @method onTitleBar
+   */
+  onTitleBar(e) {
+    /*my.alert({
+      title: '温馨提示',
+      content: '您点击了"我是手机标题栏"'
+    });*/
+  },
+  go_back(){
+    
+      my.redirectTo({
+        url: '../agreement/agreement', // 需要跳转的应用内非 tabBar 的目标页面路径 ,路径后可以带参数。参数规则如下：路径与参数之间使用
+
+      });
+    
+  }
+
+  
+});
